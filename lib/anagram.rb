54dc254805be32d94142b10e3dd6b5d233b8e9a7
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-09-20 18:06:44 -0500

    commit tree ada52acfeda2d60f890bd716439d04c61b74e1b1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array)
+        array.select do 
+            |letter| letter.split("").sort == @name.split("").sort 
+        end
+    end
+end
